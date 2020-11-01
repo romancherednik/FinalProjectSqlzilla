22a385f22b3e9f2f4d5052eb48eedc9ccb2a02fd
Jeffry Babb <ahuimanu@gmail.com>
2020-11-01 13:42:54 -0600

    modified pip

--- blog_deploy/settings.py
+++ blog_deploy/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['buffteks.net', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
