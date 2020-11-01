9604b81cbe3bcc16434a5ffc1e02b91b95931a85
Jeffry Babb <ahuimanu@gmail.com>
2020-11-01 14:27:47 -0600

    enabled static

--- blog_deploy/settings.py
+++ blog_deploy/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,3 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
+# STATICFILES_DIRS = ()
